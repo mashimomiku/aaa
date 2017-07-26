fd52d105a14ecd6e86f30aab0dcc67c6e97fea0f
mashimomiku <miku.kurusu@gmail.com>
2017-07-26 20:32:21 +0900

    Create test.m

---
+++ test.m
@@ -0,0 +1,7 @@
+週報 =======================================================  
+* 週: 7週(2017/07/13〜07/27)
+* 学籍番号: 1730097 
+* 氏名: 眞下美紅
+週報内容----------------------------------------------------  
+1. <img src="https://raw.githubusercontent.com/mashimomiku/ScSR/master/SR34.267392.bmp">
+2. ![エビフライトライアングル](https://raw.githubusercontent.com/mashimomiku/ScSR/master/SR34.267392.bmp "サンプル")
